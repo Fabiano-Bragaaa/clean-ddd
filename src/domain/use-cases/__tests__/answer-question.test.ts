899caeabf2a789a2ef77dfc288a55a78ffceb8a3
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-09-29 21:45:36 -0300

    Dependências externas

--- src/domain/use-cases/__tests__/answer-question.test.ts
+++ src/domain/use-cases/__tests__/answer-question.test.ts
@@ -1,10 +1,18 @@
 import {expect, it} from 'vitest'
 import { AnswerQuestionUseCase } from '../answer-question';
+import { Answer } from '@/domain/entities/answer';
+import { AnswersRepository } from '@/repositories/answers-repository';
 
-it('should be able to answer a question', () => {
-  const answerQuestionUseCase = new AnswerQuestionUseCase();
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+}
 
-  const answer = answerQuestionUseCase.execute({
+it('should be able to answer a question', async () => {
+  const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestionUseCase.execute({
     instructorId: '1',
     questionId: '1',
     content: 'Answer to question',
