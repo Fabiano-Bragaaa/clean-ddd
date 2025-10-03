45e0f72343d549a618774eb442f8673bed08a303
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-10-02 21:10:50 -0300

    Gerando dados fictícios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,23 @@
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { faker } from "@faker-js/faker";
 import {
   Question,
   QuestionProps,
-} from '@/domain/forum/enterprise/entities/question'
+} from "@/domain/forum/enterprise/entities/question";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Question',
-    content: 'Question content',
-    authorId: new UniqueEntityId('1'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      authorId: new UniqueEntityId("1"),
+      ...override,
+    },
+    id
+  );
 
-  return question
+  return question;
 }
