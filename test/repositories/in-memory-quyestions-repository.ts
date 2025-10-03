91acabeaff356814af7ae29fb7459b655eafa821
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-10-03 07:58:46 -0300

    Caso de uso: Listar perguntas recentes

--- test/repositories/in-memory-quyestions-repository.ts
+++ test/repositories/in-memory-quyestions-repository.ts
@@ -1,43 +1,53 @@
-import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
-import { Question } from '@/domain/forum/enterprise/entities/question'
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import { QuestionRepository } from "@/domain/forum/application/repositories/question-repository";
+import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionRepository {
-  
-  public questions: Question[] = []
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    return this.questions
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
+  }
+
+  public questions: Question[] = [];
 
   async create(question: Question): Promise<void> {
-    this.questions.push(question)
+    this.questions.push(question);
   }
 
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.questions.find(
-      (question) => question.slug.value === slug,
-    )
+      question => question.slug.value === slug
+    );
 
     if (!question) {
-      return null
+      return null;
     }
 
-    return question
+    return question;
   }
 
   async delete(question: Question): Promise<void> {
     const questionIndex = this.questions.findIndex(
-      (question) => question.id === question.id,
-    )
-    this.questions.splice(questionIndex, 1)
+      question => question.id === question.id
+    );
+    this.questions.splice(questionIndex, 1);
   }
 
   async findById(id: string): Promise<Question | null> {
-    const question = this.questions.find((question) => question.id.toString() === id)
+    const question = this.questions.find(
+      question => question.id.toString() === id
+    );
     if (!question) {
-      return null
+      return null;
     }
-    return question
+    return question;
   }
 
   async save(question: Question): Promise<void> {
-    const questionIndex = this.questions.findIndex((question) => question.id === question.id)
-    this.questions[questionIndex] = question
+    const questionIndex = this.questions.findIndex(
+      question => question.id === question.id
+    );
+    this.questions[questionIndex] = question;
   }
 }
