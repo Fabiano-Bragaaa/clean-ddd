d6ffab872f834a1bb5153e37bd33ffa96932f0ce
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-10-08 22:04:07 -0300

    Testando classes de erro

---
+++ src/core/either.test.ts
@@ -0,0 +1,21 @@
+import { Either, left, right } from "./either"
+
+function doSomething(shouldSuccess: boolean): Either<string, string> {
+  if (shouldSuccess) {
+    return right('success')
+  } else {
+    return left('error')
+  }
+}
+test('success result', () => {
+  const success = doSomething(true)
+  expect(success.isRight()).toBe(true)
+  expect(success.isLeft()).toBe(false)
+})
+
+test('error result', () => {
+  const error = doSomething(false)
+
+  expect(error.isRight()).toBe(false)
+  expect(error.isLeft()).toBe(true)
+})
